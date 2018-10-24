b0483023968bfc5996e27719f75cf714c5482dbc
Sander Larsen <s4nd3rs4l4m4nd3r@gmail.com>
2018-10-24 12:05:44 +0200

    Update db_conf.php

--- core/classes/db_conf.php
+++ core/classes/db_conf.php
@@ -5,10 +5,10 @@
  */
 class db_conf extends db {
     function __construct() {
-        $this->dbhost = "sql.itcn.dk:3306";
-        $this->dbuser = "sand4782.SKOLE";
-        $this->dbpassword = "7gAF41fgW1";
-        $this->dbname = "sand47823.SKOLE";
+        $this->dbhost = "host";
+        $this->dbuser = "user";
+        $this->dbpassword = "password";
+        $this->dbname = "dbname";
         $db = parent::connect();
     }
 }
